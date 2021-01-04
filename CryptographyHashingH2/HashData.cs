01186a8df08033ef2600d44336aaf9cb1eb5d8df
Kenneth Folke Larsen <5504355@RTS-365.dk>
2021-01-04 12:49:44 +0100

    Add project files.

---
+++ CryptographyHashingH2/HashData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptographyHashingH2
+{
+    class HashData
+    {
+
+
+        public static byte[] ComputeHmacsha256(byte[] toBeHashed, byte[] key)
+        {
+            using (var hmac = new HMACSHA256(key))
+            {
+                return hmac.ComputeHash(toBeHashed);
+            }
+        }
+
+        public static byte[] ComputeHmacsha1(byte[] toBeHashed, byte[] key)
+        {
+            using (var hmac = new HMACSHA1(key))
+            {
+                return hmac.ComputeHash(toBeHashed);
+            }
+        }        
+
+        public static byte[] ComputeHmacmd5(byte[] toBeHashed, byte[] key)
+        {
+            using (var hmac = new HMACMD5(key))
+            {
+                return hmac.ComputeHash(toBeHashed);
+            }
+        }
+    }
+}
---
+++ CryptographyHashingH2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptographyHashingH2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter text to hash");
+                string hashString = Console.ReadLine();
+                Console.WriteLine("Enter key");
+                string userKey = Console.ReadLine();
+
+                Console.WriteLine("Choose encryption form \n" +
+                    "1. Sha256\n" +
+                    "2. MD5\n" +
+                    "3. Sha1");
+                string encryptMethod = Console.ReadLine().ToLower();
+
+
+                Console.WriteLine("is this validation? y/n");
+                bool isValidation = Console.ReadLine().ToLower() == "y" ? true : false;
+                if (isValidation)
+                {
+                    Console.WriteLine("Enter hashed text");
+                    string hashedText = Console.ReadLine();
+                    Console.WriteLine("Your inserted text:\n" + hashedText);
+                }
+
+                switch (encryptMethod)
+                {
+                    case "sha256":
+                    case "1":
+                        Console.WriteLine("Hashed value: " + Convert.ToBase64String(HashData.ComputeHmacsha256(Encoding.UTF8.GetBytes(hashString), Encoding.UTF8.GetBytes(userKey))));
+                        break;
+                    case "md5":
+                    case "2":
+                        Console.WriteLine("Hashed value: " + Convert.ToBase64String(HashData.ComputeHmacmd5(Encoding.UTF8.GetBytes(hashString), Encoding.UTF8.GetBytes(userKey))));
+                        break;
+                    case "sha1":
+                    case "3":
+                        Console.WriteLine("Hashed value: " + Convert.ToBase64String(HashData.ComputeHmacsha1(Encoding.UTF8.GetBytes(hashString), Encoding.UTF8.GetBytes(userKey))));
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
